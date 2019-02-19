3d0f22055c3f6061d7b6144cd4396cc1d36a819e
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-19 21:13:32 +0100

    delete useless key prop

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
 class Article extends Component {
+
+  state = {
+    isInEditMode: false
+  }
+
   render() {
     return (
-      <div key={this.props.article.id}>
+      <div>
         <button onClick={() => this.props.editArticle()}>Edit</button>
         {this.props.article.quantity} {this.props.article.name}
         <button onClick={() => this.props.removeArticle(this.props.article)}>Delete</button>
