67f6850eb8aa1dbb7ee4efdf7a764f78e5c1f42b
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-19 19:42:50 +0100

    fix class error & initialize redux store & mock reducer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+
+const mockReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'TEST':
+      console.log('TEST reducer')
+      return state
+    default:
+      return state
+  }
+}
+
+const store = createStore(combineReducers({ mock: mockReducer }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'))
