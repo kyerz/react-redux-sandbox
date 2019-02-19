1a05a87a0987bf24e1e708511f279e8996118a94
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-19 21:11:54 +0100

    convert Article component to class

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Article = ({ article, removeArticle, editArticle }) => {
-  return (
-    <div key={article.id}>
-      <button onClick={() => editArticle()}>test editArticle</button>
-      {article.quantity} {article.name}
-      <button onClick={() => removeArticle(article)}>Delete</button>
-    </div>
-  )
+class Article extends Component {
+  render() {
+    return (
+      <div key={this.props.article.id}>
+        <button onClick={() => this.props.editArticle()}>Edit</button>
+        {this.props.article.quantity} {this.props.article.name}
+        <button onClick={() => this.props.removeArticle(this.props.article)}>Delete</button>
+      </div>
+    )
+  }
 }
+
 
 export default Article
