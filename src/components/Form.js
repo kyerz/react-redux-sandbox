18ba04111e4dc07b36aec3dc0cba0c5cb60f838f
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-19 20:07:22 +0100

    change Form to class component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Form = () => {
-  return (
-    <form>
-      <input type="number" placeholder="quantity" />
-      <input type="text" placeholder="article" />
-    </form>
-  )
+class Form extends Component {
+  state = {
+    name: '',
+    quantity: 0
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+    this.setState({ name: '', quantity: 0 })
+  }
+
+  render() {
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="number" placeholder="quantity" onChange={(e) => this.setState({ quantity: e.target.value })} value={this.state.quantity} />
+        <input type="text" placeholder="article" onChange={(e) => this.setState({ name: e.target.value })} value={this.state.name} />
+        <button type="submit">Add to cart</button>
+      </form >
+    )
+  }
 }
 
 export default Form
